cd1cddb701f5b670c6a5c153192e6cbf675d5a57
dbjerry <kimjitae91@gmail.com>
2019-04-09 15:58:54 +0900

    2019-04-09 15:58 updating... scroll

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
+const body = document.querySelector('body');
+const section = body.querySelector('.section');
 
+function handleWheel(event){
+  console.log(event);
+}
 
 function init(){
-
+  // body.addEventListener('window.onmousewheel', handleWheel);
+  window.onmousewheel = function(e){
+    console.dir(e);
+    if(e.wheelDelta === -120){
+      console.log('wheel down');
+    } else {
+      console.log('wheel up');
+    }
+  }
 }
 
 init();

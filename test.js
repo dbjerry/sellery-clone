93d8c220ea394a051c86f143800ea7f7863448f5
dbjerry <kimjitae91@gmail.com>
2019-04-09 17:54:13 +0900

    2019-04-09 17:53 I am angry...

--- test.js
+++ test.js
@@ -1,20 +1,36 @@
 const body = document.querySelector('body');
 const section = body.querySelector('.section');
+// const fullPage = section.parentNode;
 
-function handleWheel(event){
-  console.log(event);
-}
+/* const onMouseOver = section.onmouseover = function(e){
+  handleMouseOver(e);
+} */
 
-function init(){
-  // body.addEventListener('window.onmousewheel', handleWheel);
+/* function handleMouseOver(e){
+  const target = e.target.classList[0];
+  const targetParent = document.querySelector(`.${target}`).parentNode;
+  console.log(targetParent === fullPage.firstElementChild);
+  if(targetParent === fullPage.firstElementChild){
+    console.log('Top of top is here');
+  } else {
+    onMouseWheel();
+  }
+} */
+
+function onMouseWheel(){
   window.onmousewheel = function(e){
-    console.dir(e);
     if(e.wheelDelta === -120){
       console.log('wheel down');
     } else {
       console.log('wheel up');
+      scrollTo(document.querySelector('.sc2'), 'down');
     }
   }
+}
+
+function init(){
+  onMouseWheel();
+  // onMouseOver();
 }
 
 init();

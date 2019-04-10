6aca6bb4ea1d29d4c95d1b09882db2bfefd509f5
dbjerry <kimjitae91@gmail.com>
2019-04-10 14:04:55 +0900

    2019-04-10 14:04 update js

--- js/scroll.js
+++ js/scroll.js
@@ -1,37 +1,59 @@
-const body = document.querySelector('body');
-const section = body.querySelector('.section');
-// const fullPage = section.parentNode;
+const spec_section_parent = document.querySelector('.section').parentNode;
+const spec_section = document.querySelector('.section');
+const CLASSNAME_NUM = 1;
 
-/* const onMouseOver = section.onmouseover = function(e){
-  handleMouseOver(e);
-} */
-
-/* function handleMouseOver(e){
-  const target = e.target.classList[0];
-  const targetParent = document.querySelector(`.${target}`).parentNode;
-  console.log(targetParent === fullPage.firstElementChild);
-  if(targetParent === fullPage.firstElementChild){
-    console.log('Top of top is here');
-  } else {
-    onMouseWheel();
-  }
-} */
-
-function onMouseWheel(){
+function onMouseWheel(name){
+  let previousDiv = name.previousElementSbling;
+  let nextDiv = name.nextElementSibling;
   window.onmousewheel = function(e){
     if(e.wheelDelta <= 0){
-      console.log('wheel down');
-      window.onscroll(-window.innerHeight);
+      console.log(nextDiv, 'wheel down');
+      spec_section.animate({scrollTop : nextDiv.offsetTop}, 400);
     } else if(e.wheelDelta >= 0) {
-      console.log('wheel up');
-      console.log(window.innerHeight); // 722
+      console.log(previousDiv, 'wheel up');
     }
+    // window.innerHeight <-- 일단 알아두기
   }
+}
+
+const mouseOver = function onMouseOver(e){
+  const targetParent = e.target.parentNode; // 마우스 현재 위치의 부모 DOM
+  if(targetParent.className.includes(targetParent.classList[CLASSNAME_NUM])){
+    onMouseWheel(targetParent); // 현재 마우스의 div 위치
+  }
+}
+
+function getCurrentDivClassName(){
+  window.onmouseover = mouseOver;
 }
 
 function init(){
-  // onMouseWheel();
-  // onMouseOver();
+  getCurrentDivClassName();
 }
 
 init();
+
+/* 
+ * https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+ *
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  // Do something with the scroll position
+  console.log(scroll_pos);
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  console.log('scroll');
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+      console.log('!ticking : before');
+    });
+    console.log('!ticking : after');
+    ticking = true;
+  }
+}); */

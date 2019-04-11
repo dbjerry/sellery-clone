22d01f53d9add80e02ebee9678f29dbdf67b945e
dbjerry <kimjitae91@gmail.com>
2019-04-11 15:31:42 +0900

    2019-04-11 15:31 this is that check if there is a price in the salary input.

--- js/amount.js
+++ js/amount.js
@@ -1,11 +1,30 @@
-const div_section1 = document.querySelector('#section1');
-
-function fn_onmouseenter(){
-  console.log('div_section1:\n', div_section1);
+/**
+ * this is that check if there is a price in the salary input.
+ */
+function checkInputValue(t){
+  if(t[1].value === ''){
+    fullpage_api.setAllowScrolling(false);
+  } else {
+    fullpage_api.setAllowScrolling(true);
+  }
 }
 
+/**
+ * this section that I see now.
+ */
+function getCurrentSection(e){
+  if(e.target.parentNode.id === 'section1'){
+    const div_target = e.target.getElementsByTagName('input');
+    checkInputValue(div_target);
+  }
+}
+
+/**
+ * initialization
+ */
 function init(){
-  addEventListener('click', fn_onmouseenter);
+  window.onmouseover = getCurrentSection; // ex. section0, section1 ...
+  // fn_section1_input_value_check();
 }
 
-init();
+init(); // call the init().
